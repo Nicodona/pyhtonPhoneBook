785e8d82f4bbabdb87b498c461a432b9c007121e
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-04 22:36:41 +0100

    [phonebook.py]: create group

--- phonebook.py
+++ phonebook.py
@@ -28,3 +28,34 @@ class Contact:
         close_connection()
         print("Contact successfuly created")
         pass
+
+class Group:
+    def __init__(self) -> None:
+        print("Creating a group....")
+        pass
+
+    def create(self, name, desc):
+        add_group = ("INSERT INTO group "
+                     "(name, description) "
+                     "VALUES (%s, %s)")
+        # Insert group information
+        data_group = {
+            'name': name,
+            'description': desc
+        }
+        cursor.execute(add_group, data_group)
+
+        # Make sure data is committed to the database
+        close_connection()
+        print("Group successfuly created")
+        pass
+
+    def delete(self, id):
+        pass
+
+    def update(self, id):
+        pass
+
+    def get_groups(self):  # return all groups
+        pass
+
