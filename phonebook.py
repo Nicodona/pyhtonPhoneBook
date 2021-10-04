fd8d11a6c98f3da913e8199682676134279c58bb
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-04 22:36:10 +0100

    [phonebook.py]: create contact query

--- phonebook.py
+++ phonebook.py
@@ -6,3 +6,25 @@ try:
     cursor = cnx.cursor()
 except AttributeError as err:
     print(err)
+
+
+class Contact:
+    def __init__(self) -> None:
+        print("Creating contact..")
+        pass
+
+    def create(self, name, tel, groupID):
+        add_contact = ("INSERT INTO contact "
+                       "(name, tel, groupID) "
+                       "VALUES (%s, %s, %s)")
+        data_contact = {
+            'name': name,
+            'tel': tel,
+            'groupID': groupID
+        }
+        cursor.execute(add_contact, data_contact)
+
+        # Make sure data is committed to the database
+        close_connection()
+        print("Contact successfuly created")
+        pass
